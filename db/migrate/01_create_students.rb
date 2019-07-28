80c395709024c9abdc06317e383a3198efb7705c
Tim Knoll <timothy.knoll@gmail.com>
2019-07-28 03:00:40 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,6 +1,6 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
 
-  def change #creating a table
+  def change #creating a table and student + name
     create_table :students do |t|
       t.string :name
     end   
